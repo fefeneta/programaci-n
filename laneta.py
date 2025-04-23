395c7573980f72ede5770d873497d9e8612d75f0
feferico <federicocalaio72@gmail.com>
2025-04-23 13:06:42 -0300

    ejercicio numero 3
    este fue mucho esfuerzo y sudor

--- laneta.py
+++ laneta.py
@@ -4,11 +4,25 @@ def main() :
     ventana.title ("ejercicio 2")
     ventana.geometry("700x700")
     ventana.configure(bg="lightblue")
+    etiqueta1 = tk.Label(ventana, text="ejercicio 2", font=("arial", 16))
+    etiqueta1.pack()
+    etiqueta2 = tk.Label(ventana, text="Ingrese su nombre:", font=("arial", 16))
+    etiqueta2.pack()
+    campo=tk.Entry (ventana)
+    campo.pack()
     
-    etiqueta = tk.Label(ventana, text="ejercicio 2", font=("arial", 16))
-    etiqueta.pack()
-    etiqueta = tk.Label(ventana, text="federico", font=("arial", 16))
-    etiqueta.pack()
+    
+    def saludar():
+        nombre=campo.get()
+        etiqueta3 = tk.Label(ventana, text=f"hola {nombre}, te saluda fede", font=("arial", 16))
+        etiqueta3.pack()
+        
+    boton1 = tk.Button(ventana, text="saludar", command=saludar)
+    boton1.pack (pady=5)
+
+   
+    
+    
     ventana.mainloop()
 
 if __name__=="__main__":
