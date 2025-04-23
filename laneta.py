cc9e0b5f9df159defcd5ca048e7b7101af22eb4b
feferico <federicocalaio72@gmail.com>
2025-04-23 08:44:17 -0300

    ejercicio uno de fede
    esta nashe el trabajo

---
+++ laneta.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+def main () :
+    #crear ventana principal
+    ventana = tk.Tk ()
+    ventana.title ("fefeneta")
+    ventana.geometry("500x400")
+    ventana.configure(bg="black")
+    ventana.mainloop ()
+
+if __name__=="__main__":
+    main()
